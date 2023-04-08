0a13ef11ddb3f03138a0d1339b039cd2bd50ef4f
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-04-07 22:19:16 -0300

    feat: generate dataframe from sinais-results query

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ aloquiro_results: list[tuple] = cursor.execute('SELECT "ABREVIATURA" FROM "ALOQU
 sinais_results: list[tuple] = cursor.execute('SELECT "MOVIMENTOS" FROM "SINAIS";').fetchall()
 
 dataframe_columns = [record[0] for record in aloquiro_results]
-dataframe_data = [record[0] for record in sinais_results]
-for record in sinais_results:
-    signal = record[0].replace('*OK', '').strip().split(' ') # cutting the *OK ending from the signal
-    print(signal)
 
 dataframe = pandas.DataFrame([[0]*501], columns=dataframe_columns)
+
+for record in sinais_results:
+    signal = record[0].replace('*OK', '').strip().split(' ') # cutting the *OK ending from the signal
+    dataframe.loc[len(dataframe)] = [1 if x in signal else 0 for x in dataframe_columns]
+    print(signal)
 
 print(dataframe)
