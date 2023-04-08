2028ea1306e160d0f52fe1923155d3f8022c244b
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-04-07 21:59:53 -0300

    refactor: extract columns to a variable

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ cursor = conn.cursor()
 aloquiro_results: list[tuple] = cursor.execute('SELECT "ABREVIATURA" FROM "ALOQUIRO";').fetchall()
 sinais_results: list[tuple] = cursor.execute('SELECT "MOVIMENTOS" FROM "SINAIS";').fetchall()
 
-dataframe = pandas.DataFrame([[0]*501], columns=[record[0] for record in aloquiro_results])
+dataframe_columns = [record[0] for record in aloquiro_results]
+for record in sinais_results:
+    print(record)
+
+dataframe = pandas.DataFrame([[0]*501], columns=dataframe_columns)
 
 print(dataframe)
