33d74e6a315d49c712e36cc434394efb6c2169ba
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-04-07 22:06:43 -0300

    chore: variabel signal extracting OK

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ aloquiro_results: list[tuple] = cursor.execute('SELECT "ABREVIATURA" FROM "ALOQU
 sinais_results: list[tuple] = cursor.execute('SELECT "MOVIMENTOS" FROM "SINAIS";').fetchall()
 
 dataframe_columns = [record[0] for record in aloquiro_results]
+dataframe_data = [record[0] for record in sinais_results]
 for record in sinais_results:
-    print(record)
+    signal = record[0].replace('*OK', '') 
+    print(signal)
 
 dataframe = pandas.DataFrame([[0]*501], columns=dataframe_columns)
 
