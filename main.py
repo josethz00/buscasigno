34cf9ac95f6f85d61cb1b09c5ef91908a59afd99
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-04-07 21:57:20 -0300

    refactor: change aloquiro query name

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ conn = sqlite3.connect('buscasigno.db')
 cursor = conn.cursor()
 
 # lendo os dados
-results: list[tuple] = cursor.execute('SELECT "ABREVIATURA" FROM "ALOQUIRO";').fetchall()
-dataframe = pandas.DataFrame([[0]*501], columns=[record[0] for record in results])
+aloquiro_results: list[tuple] = cursor.execute('SELECT "ABREVIATURA" FROM "ALOQUIRO";').fetchall()
+dataframe = pandas.DataFrame([[0]*501], columns=[record[0] for record in aloquiro_results])
 
 print(dataframe)
