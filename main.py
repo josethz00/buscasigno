e0c92e8790a818cbed70461288da6541f58a5888
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-04-07 23:22:54 -0300

    dynamic columns

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def create_aloquiros_binary_dataframe():
 
     dataframe_columns = [record[0] for record in aloquiro_results]
 
-    dataframe = pandas.DataFrame([[0]*501], columns=dataframe_columns)
+    dataframe = pandas.DataFrame([[0]*len(aloquiro_results)], columns=dataframe_columns)
 
     for record in sinais_results:
         signal = record[0].replace('*OK', '').strip().split(' ') # cutting the *OK ending from the signal
@@ -26,19 +26,17 @@ def create_aloquiros_binary_dataframe():
 
 def create_sematosema_binary_dataframe():
     # lendo os dados
-    aloquiro_results: list[tuple] = cursor.execute('SELECT "ABREVIATURA" FROM "ALOQUIRO";').fetchall()
-    sematosema_results: list[tuple] = cursor.execute('SELECT "MOVIMENTOS" FROM "SEMATOSEMA";').fetchall()
+    sematosema_results: list[tuple] = cursor.execute('SELECT "ABREVIATURA" FROM "SEMATOSEMA";').fetchall()
+    sinais_results: list[tuple] = cursor.execute('SELECT "MOVIMENTOS" FROM "SINAIS";').fetchall()
 
-    dataframe_columns = [record[0] for record in aloquiro_results]
+    dataframe_columns = [record[0] for record in sematosema_results]
 
-    dataframe = pandas.DataFrame([[0]*501], columns=dataframe_columns)
+    dataframe = pandas.DataFrame([[0]*len(sematosema_results)], columns=dataframe_columns)
 
     for record in sematosema_results:
-        signal = record[0].replace('*OK', '').strip().split(' ') # cutting the *OK ending from the signal
-        dataframe.loc[len(dataframe)] = [1 if x in signal else 0 for x in dataframe_columns]
-        print(signal)
+        print(record)
 
-    dataframe = dataframe.tail(-1) # removing the first row, which is all zeros
+    # dataframe = dataframe.tail(-1) # removing the first row, which is all zeros
     print(dataframe)
 
     # exporting the dataframe to csv and excel
@@ -46,4 +44,5 @@ def create_sematosema_binary_dataframe():
     dataframe.to_excel(r'buscasigno-sematosema-binarydata.xlsx', index=False)
 
 if __name__ == '__main__':
-    create_aloquiros_binary_dataframe()
+    # create_aloquiros_binary_dataframe()
+    create_sematosema_binary_dataframe()
