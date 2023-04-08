2ac1bad7b65cd666d6ea08f095a684500bc9f382
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-04-07 21:47:23 -0300

    feat: create dataframe structure

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import sqlite3
+import sqlite3, pandas
 
 conn = sqlite3.connect('buscasigno.db')
 cursor = conn.cursor()
 
 # lendo os dados
-res = cursor.execute('SELECT * FROM "SINAIS";').fetchall()
-print(res)
+results: list[tuple] = cursor.execute('SELECT "ABREVIATURA" FROM "ALOQUIRO";').fetchall()
+dataframe = pandas.DataFrame(results, columns=[record[0] for record in results])
+
+print(dataframe)
