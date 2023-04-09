0e1c1ad0dd12126032cf9b09b13ac71f0922eccf
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-04-09 13:25:39 -0300

    feat: create categoria binary dataframe

--- main.py
+++ main.py
@@ -45,6 +45,42 @@ def create_sematosema_binary_dataframe():
     dataframe.to_csv(r'buscasigno-sematosema-binarydata.csv', index=True, header=True)
     dataframe.to_excel(r'buscasigno-sematosema-binarydata.xlsx', index=False)
 
+def create_categoria_binary_dataframe():
+    # lendo os dados
+    categoria_results: list[tuple] = cursor.execute('SELECT "NOME" FROM "CATEGORIA";').fetchall()
+    sinais_results: list[tuple] = cursor.execute('SELECT "MOVIMENTOS" FROM "SINAIS";').fetchall()
+
+    dataframe_columns = [record[0] for record in categoria_results]
+
+    dataframe = pandas.DataFrame([[0]*len(categoria_results)], columns=dataframe_columns)
+
+    for record in sinais_results:
+        signal: str = record[0].replace('*OK', '').strip() # cutting the *OK ending from the signal
+        match signal.split():
+            # AMD, AME, OPD, OPF, OMD, OME, RM - Mãos
+            case [*_, "AMD"] | [*_, "AME"] | [*_, "OPD"] | [*_, "OPF"] | [*_, "OMD"] | [*_, "OME"] | [*_, "RM"]:
+                dataframe.loc[len(dataframe)] = [1 if x == "Mãos" else 0 for x in dataframe_columns]
+            # QDD, QDE, ADD, ADE - Dedos
+            case [*_, "QDD"] | [*_, "QDE"] | [*_, "ADD"] | [*_, "ADE"]:
+                dataframe.loc[len(dataframe)] = [1 if x == "Dedos" else 0 for x in dataframe_columns]
+            # CP, RF, TA, PB - Local da Articulação
+            case [*_, "CP"] | [*_, "RF"] | [*_, "TA"] | [*_, "PB"]:
+                dataframe.loc[len(dataframe)] = [1 if x == "Local da Articulação" else 0 for x in dataframe_columns]
+            # MMD, MME, TMD, TME, MDD, MDE, FI, MC - Movimento
+            case [*_, "MMD"] | [*_, "MME"] | [*_, "TMD"] | [*_, "TME"] | [*_, "MDD"] | [*_, "MDE"] | [*_, "FI"] | [*_, "MC"]:
+                dataframe.loc[len(dataframe)] = [1 if x == "Movimento" else 0 for x in dataframe_columns]
+            # SSP, SSN, EMF - Expressão Facial
+            case [*_, "SSP"] | [*_, "SSN"] | [*_, "EMF"]:
+                dataframe.loc[len(dataframe)] = [1 if x == "Expressão Facial" else 0 for x in dataframe_columns]
+        print(signal)
+
+    dataframe = dataframe.tail(-1) # removing the first row, which is all zeros
+    print(dataframe)
+
+    # exporting the dataframe to csv and excel
+    dataframe.to_csv(r'buscasigno-sematosema-binarydata.csv', index=True, header=True)
+    dataframe.to_excel(r'buscasigno-sematosema-binarydata.xlsx', index=False)
+
 if __name__ == '__main__':
     create_aloquiros_binary_dataframe()
     create_sematosema_binary_dataframe()
